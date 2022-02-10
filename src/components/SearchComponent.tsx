6885062e74596651eee7b12dd1644d4b27f04735
RJ <rjsarmiento243@gmail.com>
2022-02-10 19:11:07 +0800

    fix style

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -30,6 +30,7 @@ export default function SearchComponent() {
         } catch (error) {
             console.log(error);
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [Gh.name]);
 
     return (
